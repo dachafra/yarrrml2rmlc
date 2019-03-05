61c1b1ea8c5a74262825796741d1b62c6200516c
David Chaves <dachafra@gmail.com>
2019-03-05 17:26:44 +0100

    rmlc mapping as example

--- src/main/java/es/upm/dia/oeg/App.java
+++ src/main/java/es/upm/dia/oeg/App.java
@@ -5,9 +5,6 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 /**
  * Hello world!
