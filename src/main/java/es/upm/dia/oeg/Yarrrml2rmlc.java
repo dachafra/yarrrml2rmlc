8d438359a8054f754f350d1cff598fb6146604a0
David Chaves <dachafra@gmail.com>
2019-03-22 19:13:39 +0100

    change rmlc uri

--- src/main/java/es/upm/dia/oeg/Yarrrml2rmlc.java
+++ src/main/java/es/upm/dia/oeg/Yarrrml2rmlc.java
@@ -30,7 +30,7 @@ public class Yarrrml2rmlc {
                 String value = entry.getValue();
                 rmlcContent.append("@prefix " + key + ": <" + value + ">.\n");
             }
-            rmlcContent.append("@prefix rmlc: <http://www.oeg-upm.net/ns/rmlc#>\n");
+            rmlcContent.append("@prefix rmlc: <http://www.ex.org/ns/rmlc#>\n");
             rmlcContent.append("@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n");
             rmlcContent.append("@base <http://example.org/>\n");
         }
